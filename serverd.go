4be40cba395d2c78dc6fa9f34c8b6c8491b0424c
Егор Марков <r5railmodels@gmail.com>
2025-03-26 23:42:42 +0300

    working on serverd

--- serverd.go
+++ serverd.go
@@ -1,3 +1,53 @@
 // contain types and functions to work with unix socket
 
 package main
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	"net"
+	"time"
+)
+
+const UnixSocketNet = "unix"
+
+const DefaultUnixSockTimeout = 10 * time.Second
+
+func sockclose(log *logrus.Logger, sock net.Listener) {
+	err := sock.Close()
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+// HandleCommands from fsyncdctl
+func HandleCommands(
+	ctx context.Context,
+	log *logrus.Logger,
+	sockPath string,
+) error {
+	// create server unix socket
+	servCfg := net.ListenConfig{
+		KeepAlive: DefaultUnixSockTimeout, // set timeout for ctl connection
+	}
+
+	sock, err := servCfg.Listen(ctx, UnixSocketNet, sockPath)
+	if err != nil {
+		log.Error(err)
+	}
+
+	defer sockclose(log, sock)
+
+	// handle commands
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			// more readable
+			break
+		}
+
+		// handle clients
+	}
+}
